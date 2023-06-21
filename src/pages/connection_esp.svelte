bdfcf4781a2ab7344c5d356a270a7bd72efd119f
JOCKERHUB <ironsuper99@gmail.com>
2023-06-21 02:40:02 +0200

    continuo esp

--- src/pages/connection_esp.svelte
+++ src/pages/connection_esp.svelte
@@ -4,20 +4,60 @@
     
     <div class="container-green">
         <img src="/freccio.png" id="freccia" alt="freccia">
+        <h1>Connessione</h1>
+    </div>
+
+    <!-- elenco esp -->
+
+    <div class="container-esp">
+
+        <div class="block">
+            <h2>AgroSmart v1</h2>
+            <img src="/agrosmart.png" alt="photo">
+        </div>
+
     </div>
 </Page>
-
-<style>
-    .container-green{
-        background-color: greenyellow;
-    }
-    #freccia{
-        width: 10%;
-        height: 10%;
-    }
-
-</style>
 
 <script>
     import {Page,Block,Navbar,Button} from 'framework7-svelte';
 </script>
+
+
+<style>
+
+    h1{
+        color: white;
+        font-size: 35px;
+        padding-left: 10px;
+    }
+    .container-green{
+        background-image: linear-gradient(to right,rgba(97, 210, 196, 1), rgba(41, 216, 144, 1));
+        height: 145px;
+        display: flex;
+        align-items: center;
+        
+    }
+    #freccia{
+        width: 35px;
+        height: 35px;
+        margin-left: 20px;
+    }
+    .container-esp{
+        display: flex;
+        align-items: left;
+        justify-content: left;
+        flex-direction: row;
+        
+    }
+    img{
+        width: 90px;
+        height: 90px;
+        margin-top: 40px;
+    }
+        
+
+
+
+</style>
+
