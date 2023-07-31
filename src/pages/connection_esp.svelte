4ff5f37e096be44555caf76da6741b11a0e10bbb
gabriele <gabrielevallosio@gmail.com>
2023-07-31 22:08:29 +0200

    add network capacitor

--- src/pages/connection_esp.svelte
+++ src/pages/connection_esp.svelte
@@ -9,58 +9,42 @@
 
     <!-- elenco esp -->
 
-    <div class="container-esp">
+    {#each wifi as net}
+        <div class="container-esp">
 
-        <div class="block1">
-            <h5>86%</h5>
-            <h4>Potenza segnale</h4>
-            <h2>AgroSmart v1</h2>
-            <div class="block2">
-                <img src="/agrosmart.png" alt="photo">
-                <div class="block3">
-                    <button>Connetti</button>
-
+            <div class="block1">
+                <h5>{net.level}</h5>
+                <h4>RSSI:</h4>
+                <h2>{net.SSID}</h2>
+                <div class="block2">
+                    <img src="/agrosmart.png" alt="photo">
+                    <div class="block3">
+                        <button>Connetti</button>
+                    </div>
                 </div>
             </div>
         </div>
-        
-    </div>
-    <div class="container-esp">
-
-        <div class="block1">
-            <h5>86%</h5>
-            <h4>Potenza segnale</h4>
-            <h2>AgroSmart v1</h2>
-            <div class="block2">
-                <img src="/agrosmart.png" alt="photo">
-                <div class="block3">
-                    <button>Connetti</button>
-
-                </div>
-            </div>
-        </div>
-        
-    </div>
-    <div class="container-esp">
-
-        <div class="block1">
-            <h5>86%</h5>
-            <h4>Potenza segnale</h4>
-            <h2>AgroSmart v1</h2>
-            <div class="block2">
-                <img src="/agrosmart.png" alt="photo">
-                <div class="block3">
-                    <button>Connetti</button>
-
-                </div>
-            </div>
-        </div>
-        
-    </div>
+    {/each}
 </Page>
 
 <script>
-    import {Page,Block,Navbar,Button} from 'framework7-svelte';
+    import { Page, f7 } from 'framework7-svelte';
+    import { CapacitorWifi } from 'capacitorjs-plugin-wifi';
+
+    // async function checkPermissionResult() {
+    //     let result = await CapacitorWifi.checkPermission();
+    //     f7.dialog.alert(result.status)
+    // }
+
+    let wifi = [];
+
+    async function scanWifiResult() {
+        let result = await CapacitorWifi.wifiScan();
+        wifi = result.networks;
+    }
+
+    scanWifiResult();
+
 </script>
 
 
@@ -71,6 +55,7 @@
         font-size: 35px;
         padding-left: 10px;
     }
+
     .container-green{
         background-image: linear-gradient(to right,rgba(97, 210, 196, 1), rgba(41, 216, 144, 1));
         height: 145px;
@@ -78,6 +63,7 @@
         align-items: center;
         
     }
+
     #freccia{
         width: 35px;
         height: 35px;
@@ -85,6 +71,7 @@
         top: 3px;
         position: sticky;
     }
+
     .container-esp{
         display: flex;
         align-items: left;
@@ -92,35 +79,41 @@
         flex-direction: row;
         
     }
+
     .block1{
         display: flex;
         flex-direction: row-reverse;
         margin-top: 40px;
         margin-left: 20px;
     }
+
     img{
         width: 90px;
         height: 90px;
     }
+
     h2{
         position: relative;
         bottom: 24px;
         margin-left: 15px;
         color: #36455A;
     }
+
     h4{
         color: #A1A8B9;
         position: relative;
         top: 2vh;
-        right: 15vh;
+        right: 14vh;
     }
+
     h5{
         font-size: 16px;
         color: #36455A;
         position: relative;
         top: 37px;
-        right: 62%;
+        right: 56%;
     }
+
     button{
         background-color: #2DDA93;
         color: white;
