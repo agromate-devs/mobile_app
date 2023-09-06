4e06d852b6db71b5ef44c4fd1ad0807499176726
Andrea055 <vivanco11.ac@gmail.com>
2023-09-06 10:58:08 +0200

    Format

--- src/pages/myplant.svelte
+++ src/pages/myplant.svelte
@@ -1,36 +1,36 @@
 <script>
-	import {
-		Page,
-		List
-	} from 'framework7-svelte';
+	import { Page, List } from 'framework7-svelte';
 	import CustomNavbar from '../components/CustomNavbar.svelte';
 	import PiantaItem from './PiantaItem.svelte';
 	import { onMount } from 'svelte';
 	import { getCurrentUser, get_current_user_jwt } from '../lib/firebase_auth';
 
 	let piante = [];
-	
-	function cache_plants(plants) {	// Cache plants in localStorage, risparmiamo sul costo delle query
-		sessionStorage.setItem("myplants", JSON.stringify(plants));	
+
+	function cache_plants(plants) {
+		// Cache plants in localStorage, risparmiamo sul costo delle query
+		sessionStorage.setItem('myplants', JSON.stringify(plants));
 	}
-	
-	function get_plants_from_cache(){
-		const cached_plants = sessionStorage.getItem("myplants");
+
+	function get_plants_from_cache() {
+		const cached_plants = sessionStorage.getItem('myplants');
 		return cached_plants != null ? JSON.parse(cached_plants) : [];
 	}
-	
+
 	onMount(async () => {
 		const raw_plants_from_cache = get_plants_from_cache();
-		
-		if(raw_plants_from_cache.length > 0){
+
+		if (raw_plants_from_cache.length > 0) {
 			piante = raw_plants_from_cache;
-			return;	// Stop here, don't do another useless request to AWS
+			return; // Stop here, don't do another useless request to AWS
 		}
 
 		const jwt = await get_current_user_jwt();
 		const user = await getCurrentUser();
 		fetch(
-			'https://dlc52l1dnc.execute-api.eu-central-1.amazonaws.com/plant_info_api?user_id='.concat(user.uid),
+			'https://dlc52l1dnc.execute-api.eu-central-1.amazonaws.com/plant_info_api?user_id='.concat(
+				user.uid
+			),
 			{
 				headers: new Headers({
 					authorization: jwt.token,
@@ -51,7 +51,7 @@
 
 	<List dividersIos simpleList>
 		{#each piante as pianta}
-			<!-- Cambiare in temperatura dal sensore e aggiungere gli altri parametri --> 
+			<!-- Cambiare in temperatura dal sensore e aggiungere gli altri parametri -->
 			<PiantaItem name={pianta.plant_name} temp={pianta.default_temperature} />
 		{/each}
 	</List>
